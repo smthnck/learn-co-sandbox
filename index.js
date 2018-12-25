e88842efbccdcbd861199560519601c0bb7bb0d8
Nick Smith <smthnck@gmail.com>
2018-12-25 17:29:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ module.exports = { name, height
 }
 
 
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+}
+
+var myScope = outerFunction();
+
+myScope();
+//innerScope();
